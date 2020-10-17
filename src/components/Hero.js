bba1d99ba2ab83775ee309c80ee781324d30782d
Haley Myers <haleynmyers@gmail.com>
2020-10-16 22:34:13 -0500

    Styled Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+const styles = {
+  hero: {
+    background: "#3f51b5",
+    minHeight: 50,
+    fontSize: "1.2rem",
+    color: "white",
+    padding: "50px 20px"  }
+}
+
 function Hero(props) {
   return (
-    <div className="hero text-center" style={{ backgroundImage: "url(https://www.pexels.com/photo/woman-sitting-on-the-chair-using-laptop-2041629/)" }}>
+    <div className="hero text-center" style={styles.hero}>
       <h1>Employee Directory</h1>
+      <form className="search">
+        <div className="form-group">
+          <input
+            type="text"
+            name="search"
+            onChange={props.handleInput}
+            placeholder="Search by name"
+            />
+        </div>
+      </form>
     </div>
   );
 }
